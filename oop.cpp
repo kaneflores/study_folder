008ae6c01aaac084ae475df2a6e12ebf2d2fb255
kaneflores <kaneflores911@gmail.com>
2026-06-25 15:10:38 -0400

    constructor section done

--- oop.cpp
+++ oop.cpp
@@ -19,6 +19,35 @@ class Point{ // struct another way of organizing your data
             return sqrt(xDiff * xDiff + yDiff * yDiff);
 
         }
+
+        Point(){
+            cout << "Constructor invoked" << endl;
+        }
+        // Point(int a, int b){ //constructor
+        //     cout << "This has 2 parameters" << endl;
+        //     x = a;
+        //     y = b;
+
+        // }
+
+        //syntax similarites
+        // Point(int x, int y){ //constructor // if you insist on using x and y or same variables init from the start
+        //     cout << "This has 2 parameters" << endl;
+        //     this->x = x;
+        //     this->y = y;
+            
+        // }
+
+        // //constructor initializer
+        // Point(int x, int y) : x(x), y(y){} //constructor
+
+        // what does constructor help with?
+        // -- forced full initialization
+        // -- initial access to private data
+        // distance the user of the class from the actual variables (attributes)
+
+        // encapsulation -> gates accesss to sensitive variables
+
 };
 // int main(){ // without oop
 //     // points are:
@@ -63,42 +92,70 @@ class Point{ // struct another way of organizing your data
 //     return 0;
 // }
 
-int main(){ // without oop
-    // points are:
-        // (0,1)
-        // (2,2)
-        /// ...
-        /// (4,5)
+// int main(){ // oop
+//     // points are:
+//         // (0,1)
+//         // (2,2)
+//         /// ...
+//         /// (4,5)
 
-    // Point points[5];
-    // points[0].x;
+//     // Point points[5];
+//     // points[0].x;
+
+//     vector<Point> points ={{0,1},{2,2},{2,3},{3,4},{4,5}};
+
+//     const Point& p1 = points.at(0);
+//     const Point& p2 = points.at(1);
+//     cout << "Distance: " << p1.distanceTo(p2);
+//     p1.print();
+//     p2.print();
+
+//     for( auto point : points){
+//         point.print();
+//     }
+
+//     // for (int i =0; i< 5; i++){
+//     //     cout << "(" << points.at(i).x << ", " << points.at(i).y << ")" << endl;
+//     // }
+
+
+//     Point p; //class -> object (instantiation)
+//     p.x = 5;
+//     p.y = 10;
+//     cout << p.x << endl;
+
+//     Point l;
+//     l.x =40;
+//     l.y = 20;
+    
+    
+//     return 0;
+// }
+
+int main(){ // method calls in loop and constructors
 
     vector<Point> points ={{0,1},{2,2},{2,3},{3,4},{4,5}};
 
-    const Point& p1 = points.at(0);
-    const Point& p2 = points.at(1);
-    cout << "Distance: " << p1.distanceTo(p2);
-    p1.print();
-    p2.print();
+    
 
-    for( auto point : points){
-        point.print();
+    for(int i = 0; i<points.size(); i++){
+        points.at(i).print();
+
+        if (i==4){
+            cout << "end" << endl;
+            break;
+        }
+        cout << "distance to next point: " << points.at(i).distanceTo(points.at(i+1)) << endl;
     }
-
-    // for (int i =0; i< 5; i++){
-    //     cout << "(" << points.at(i).x << ", " << points.at(i).y << ")" << endl;
-    // }
-
-
-    Point p; //class -> object (instantiation)
-    p.x = 5;
-    p.y = 10;
-    cout << p.x << endl;
-
-    Point l;
-    l.x =40;
-    l.y = 20;
     
-    
+    Point p;
     return 0;
 }
+// methods vs constructors
+/*
+a constructor is a member function that is invoked everytime an object is created
+method defines the behavior/functionality of an object i.e. distanceto, print,
+
+
+
+*/
