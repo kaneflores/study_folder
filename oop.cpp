a2ac582031b560c5e6682b157c9b12b607b9afde
kaneflores <kaneflores911@gmail.com>
2026-06-25 20:38:01 -0400

    polymorphism

--- oop.cpp
+++ oop.cpp
@@ -4,6 +4,8 @@
 
 using namespace std;
 
+
+
 // class Point{ // struct another way of organizing your data
 //     public: // public data that we can access
 //         int x{};
@@ -51,18 +53,18 @@ using namespace std;
 // };
 
 class Point{ // struct another way of organizing your data
-    private: //encapsulation example 
+    protected://encap example
         int x{};
         int y{};
 
     public: // public data that we can access
-        int getX() { return x;} //encapsulation
+        int getX() const { return x;} //encapsulation
         void setX(int newX) {x=newX;}//encapsulation ...
 
-        int getY() {return y;} 
+        int getY() const {return y;} 
         void setY(int newY) {y = newY;}
 
-        void print() const {
+      virtual void print() const{
             cout << "(" << x << ", " << y << ")" << endl;
         }
 
@@ -76,10 +78,49 @@ class Point{ // struct another way of organizing your data
         Point(){
             cout << "Constructor invoked" << endl;
         }
-        // Point(int x, int y) : x(x), y(y){} //constructor
+
+        static void printPoints(const vector<Point>& points){
+            cout << "We are printing " << points.size() << " points:" << endl;
+            for(const auto& point: points){
+                point.print();
+            }
+        }
+        Point(int x, int y) : x(x), y(y){} //constructor
         // notes: constrcutor is also an example of abstraction where you hides the details of actual initialization, you are now worried about passing data now.
 
         // encapsulation -> gates accesss to sensitive variables
+
+};
+
+class XYZPoint: public Point{
+    // polymorphism is the concept of treating objects from different, derived classes as if these objects were from a common base class.
+    // x y z
+    int z;
+
+    public:
+    XYZPoint(int x, int y, int z): Point(x,y), z(z) {}
+    void print() const override {
+        // Point::print();
+       cout << "(" << x << ", " << y << ", "<< z <<
+        ")" << endl;
+    }
+};
+class City{
+    private:
+        Point location;
+        string name;
+        int population;
+    
+    public:
+        City(): location(Point()), name("unknown"), population(0){} //constructor
+        City(const Point& location, const string name, int population): location(location), name(name), population(population){}
+
+        const Point& getLocation() const {return location;}
+        void setLocation(const Point& location) {this->location = location;}
+        string& getName() {return name;}
+        void setName(const  string& name){this->name = name;}
+        int getPopulation(){return population;}
+        void setPopulation(int population) {this->population = population;}
 
 };
 // int main(){ // without oop
@@ -165,31 +206,31 @@ class Point{ // struct another way of organizing your data
 //     return 0;
 // }
 
-int main(){ // method calls in loop and constructors
+// int main(){ // method calls in loop and constructors
 
-    vector<Point> points ={{0,1},{2,2},{2,3},{3,4},{4,5}};
+//     vector<Point> points ={{0,1},{2,2},{2,3},{3,4},{4,5}};
 
     
 
-    for(int i = 0; i<points.size(); i++){
-        points.at(i).print();
+//     for(int i = 0; i<points.size(); i++){
+//         points.at(i).print();
 
-        if (i==4){
-            cout << "end" << endl;
-            break;
-        }
-        cout << "distance to next point: " << points.at(i).distanceTo(points.at(i+1)) << endl;
-    }
+//         if (i==4){
+//             cout << "end" << endl;
+//             break;
+//         }
+//         cout << "distance to next point: " << points.at(i).distanceTo(points.at(i+1)) << endl;
+//     }
     
-    Point p;
-    p.setX(5); // encapsulation
-    p.setY(10); //encapsulation.
+//     Point p;
+//     p.setX(5); // encapsulation
+//     p.setY(10); //encapsulation.
 
-    cout << p.getX() + p.getY() << endl;
-    // encapsulation is an example of abstraction 
+//     cout << p.getX() + p.getY() << endl;
+//     // encapsulation is an example of abstraction 
 
-    return 0;
-}
+//     return 0;
+// }
 // methods vs constructors
 /*
 a constructor is a member function that is invoked everytime an object is created
@@ -198,3 +239,38 @@ method defines the behavior/functionality of an object i.e. distanceto, print,
 
 
 */
+////////////////////////////////////////////////////////////////////////// static method work
+// int main(){ 
+
+//     vector<Point> points ={{0,1},{2,2},{2,3},{3,4},{4,5}};
+//     Point::printPoints(points);
+
+//     //static methods 
+//     //organization pros
+//     //point related methods, but not for a specific boject
+//     // for example, reading objects from a file
+//     //write objects to a file
+//     // aggregate data across objects
+//     return 0;
+// }
+
+// int main(){
+//     City city = City(Point(5,10), "Lumbridge", 50);
+//     // city.setLocation(Point(50,100));
+//     // city.setName("New city");
+//     city.setPopulation(500000);
+//     cout << city.getPopulation() << endl;
+//     return 0;
+// }
+
+int main(){
+    XYZPoint p1=XYZPoint(1,5,5);
+    Point p2 = Point(3,4);
+
+    vector<Point*> points = {&p1,&p2};
+    
+    for (auto p: points){
+        p->print();
+    }
+    return 0;
+}
